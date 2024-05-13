9cc2dbf2717c5b8429a99a87c58001abcb9802ff
XuHaoNan <1056795985@qq.com>
2024-05-13 23:37:39 +0800

    完善软件用户表

---
+++ new/app/controller/agent/UserClass.go
@@ -0,0 +1,50 @@
+package controller
+
+import (
+	"github.com/gin-gonic/gin"
+	"server/new/app/controller/Common"
+	"server/new/app/logic/common/userClass"
+	"server/structs/Http/response"
+	"strconv"
+)
+
+type UserClass struct {
+	Common.Common
+}
+
+func NewUserClassController() *UserClass {
+	return &UserClass{}
+}
+
+// GetAppIdNameList 取id和名字数组
+func (C *UserClass) GetIdNameList(c *gin.Context) {
+	var 请求 struct {
+		Id    int `json:"Id"`
+		AppId int `json:"AppId" binding:"required,min=10000"`
+	}
+	if !C.ToJSON(c, &请求) {
+		return
+	}
+
+	IdName := userClass.L_userClass.UserClass取map列表String(请求.AppId)
+	var 临时Int int
+	var Name []键值对
+
+	for Key := range IdName {
+		临时Int, _ = strconv.Atoi(Key)
+		Name = append(Name, 键值对{Id: 临时Int, Name: IdName[Key]})
+	}
+
+	response.OkWithDetailed(响应_AppIdNameList{IdName, Name}, "获取成功", c)
+	return
+}
+
+type 键值对 struct {
+	Id   int    `json:"id"`
+	Name string `json:"Name"`
+}
+
+type 响应_AppIdNameList struct {
+	Map   map[string]string `json:"Map"`
+	Array []键值对          `json:"Array"`
+}
