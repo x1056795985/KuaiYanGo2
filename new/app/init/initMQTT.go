fa24f8bd577f1b73f8c8a7246ae3bf26b674c97e
XuHaoNan <1056795985@qq.com>
2024-07-30 10:23:37 +0800

    ip数据库支持使用本地库 修复首次启动未连接数据库会报错的问题

--- new/app/init/initMQTT.go
+++ new/app/init/initMQTT.go
@@ -2,11 +2,16 @@ package init
 
 import (
 	"github.com/gin-gonic/gin"
+	"server/global"
 	"server/new/app/logic/common/mqttClient"
 	"server/new/app/logic/common/setting"
 )
 
 func InitMqttClient() {
+
+	if global.GVA_DB == nil {
+		return
+	}
 	mqttConfig := setting.Q取MQTT配置()
 	if mqttConfig.L连接状态 {
 		_ = mqttClient.L_mqttClient.L连接(&gin.Context{}, mqttConfig.F服务器地址, mqttConfig.F服务器端口, mqttConfig.Y用户名, mqttConfig.M密码)
