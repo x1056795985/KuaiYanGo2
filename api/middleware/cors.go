9cf3ae7907aa36b73f609283dde22ef76ae8eaca
XuHaoNan <1056795985@qq.com>
2024-10-03 20:29:37 +0800

    优化跨域问题,允许所有跨域请求头信息

--- api/middleware/cors.go
+++ api/middleware/cors.go
@@ -11,7 +11,9 @@ func Cors() gin.HandlerFunc {
 		method := c.Request.Method
 		origin := c.Request.Header.Get("Origin")
 		c.Header("Access-Control-Allow-Origin", origin)
-		c.Header("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization, Token,X-Token")
+		// 允许请求中的所有头部字段
+		c.Header("Access-Control-Allow-Headers", "*")
+		//c.Header("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization, Token,X-Token,content-md5")
 		c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS,DELETE,PUT")
 		c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type, New-Token, New-Expires-At")
 		c.Header("Access-Control-Allow-Credentials", "true")
