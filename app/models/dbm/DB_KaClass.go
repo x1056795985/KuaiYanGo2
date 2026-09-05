2d3cd79882c492a8eacd954e655adcc67c7e580e
XuHaoNan <1056795985@qq.com>
2026-09-05 19:03:32 +0800

    修复不生成只有数字的卡号问题

--- app/models/dbm/DB_KaClass.go
+++ app/models/dbm/DB_KaClass.go
@@ -15,7 +15,7 @@ type DB_KaClass struct {
 	UserClassId  int     `json:"UserClassId" gorm:"column:UserClassId;comment:用户分类id"`
 	NoUserClass  int     `json:"NoUserClass" gorm:"column:NoUserClass;comment:用户类型不同处理方式,1自动根据权重转换分组,2禁止充值"`
 	KaLength     int     `json:"KaLength" gorm:"column:KaLength;comment:卡长度"`
-	KaStringType int     `json:"KaStringType" gorm:"column:KaStringType;comment:卡内容字符集"` // 1 大小写字母+数字  2 大写字母 +数字  3 小写字母+数字
+	KaStringType int     `json:"KaStringType" gorm:"column:KaStringType;comment:卡内容字符集"` // 1 大小写字母+数字  2 大写字母 +数字  3 小写字母+数字 4 数字
 	Num          int     `json:"Num" gorm:"column:Num;comment:可以充值次数"`
 	KaType       int     `json:"KaType" gorm:"column:KaType;comment:充值类型"`        //1=无限制只要有次数,2每个账号限制一次
 	MaxOnline    int     `json:"MaxOnline" gorm:"column:MaxOnline;comment:最大在线数"` //修改可以修改App最大在线数量
